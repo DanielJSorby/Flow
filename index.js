bb093a273392583041a70811e8e2718bed281128
Loliskrem <81819141+loliskrem@users.noreply.github.com>
2024-04-28 12:41:49 +0200

    Endra på bilder, og lagde flavours

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log("index.js loaded");
+
+/* Change color flavours */
+$(document).ready(function () {
+	$(window).scroll(function () {
+		var scrollHeight = $(this).scrollTop();
+		if (scrollHeight < $(window).height()) {
+			// mindre enn 100vh
+			$(".flavours-nav a").css("color", "#fff");
+			$(".flavours-nav h1").css("color", "#fff");
+			console.log("top");
+		} else if (scrollHeight < 2 * $(window).height()) {
+			// mellom 100vh og 200vh
+			$(".flavours-nav a").css("color", "#6A8C65");
+			$(".flavours-nav h1").css("color", "#6A8C65");
+			console.log("middle");
+		} else if (scrollHeight < 3 * $(window).height()) {
+			// mellom 200vh og 300vh
+			$(".flavours-nav a").css("color", "#FFFABA");
+			$(".flavours-nav h1").css("color", "#FFFABA");
+			console.log("bottom");
+		}
+	});
+});
