59245f282d0e686b78056217d650d95eca9b3cec
Loliskrem <81819141+loliskrem@users.noreply.github.com>
2024-04-29 10:08:44 +0200

    Endra flavours til json
    La til json fil for flavours, og gjorde at javascripten lagde siden basert på innformasjonen som den får fra json filen

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 console.log("index.js loaded");
 
+fetch(".././flavours.json")
+    .then((response) => response.json())
+    .then((data) => {
+        const flavours = data;
+        console.log(flavours);
+        for (let i = 0; i < flavours.length; i++) {
+            const flavour = flavours[i];
+            if (window.location.href === "http://127.0.0.1:5500/flavours/") {
+                const flavourTemplate = createFlavourTemplate(flavour);
+                document.querySelector(".flavours").innerHTML +=
+                    flavourTemplate;
+            }
+            console.log(flavour);
+
+        }
+    });
+function createFlavourTemplate(flavour) {
+    const ingredients = flavour.ingredients ? flavour.ingredients.join('<br>') : 'No ingredients listed';
+    return `
+    <div class="flavour-container ${flavour.name}" style="background-color: ${flavour.mainColor}; color: ${flavour.secondColor}">
+        <div class="flavour">
+            <div class="flav-text-button">
+                <div class="flav-text">
+                    <h1 class="flav-name">${flavour.name}</h1>
+                    <div class="flav-desc">
+                        <p class="flav-ingredients">
+                            ${ingredients}
+                        </p>
+                        <div class="line flav-line"></div>
+                        <div class="flav-nutritions">
+                            <p class="flav-numbers">${flavour.coffeine}<br>${flavour.vitaminK}</p>
+                            <p class="flav-names">Coffeine<br>Vitamin K</p>
+                        </div>
+                    </div>
+                </div>
+                <a href="./${flavour.name}/">
+                    <div class="flav-button">
+                        <button>Purchase</button>
+                    </div>
+                </a>
+            </div>
+            <div class="flav-image">
+                <img src="../images/Bottles/${flavour.name}.png" alt="${flavour.name}" class="flav-pic">
+            </div>
+        </div>
+    </div>
+    `;
+}
+
 /* Change color flavours */
 $(document).ready(function () {
 	$(window).scroll(function () {
